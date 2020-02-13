e3ef19e79d1794eb11267d1076dc4ba40817ee17
cell <vvgrygorchuk@gmail.com>
2020-02-13 23:19:42 +0200

    Add args parser.

--- source/args_parser.py
+++ source/args_parser.py
@@ -1,7 +1,12 @@
 import argparse
 
 
-def parse_input():
+def parse_args() -> (int, int):
+    """Parse provided args for the board's width and height.
+    Width and height should be int and both are required.
+    """
     parser = argparse.ArgumentParser()
-    parser.add_argument("-", "--version", help="show program version", action="store_true")
-    return parser.parse_args()
+    parser.add_argument("-W", "--width", help="Provide the board's width.", type=int, required=True)
+    parser.add_argument("-H", "--height", help="Provide the board's height.", type=int, required=True)
+    results = parser.parse_args()
+    return results.width, results.height
